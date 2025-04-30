4864fc914d15a4c8033f180dc0774f708ae5e5df
thiagodc <141529028+thiagodcoq@users.noreply.github.com>
2025-04-29 22:05:01 -0300

    Update main.c, print by order, and print elements

--- .dist/main.c
+++ .dist/main.c
@@ -33,11 +33,51 @@ NoArv *inserir_versao_1(NoArv *raiz, int num)
     }
 }
 
+void imprimir_versao_1(NoArv*raiz){
+    if(raiz){
+        printf("%d ",raiz->valor);
+        imprimir_versao_1(raiz->esquerda);
+        imprimir_versao_1(raiz->direita);
+    }
+}
+
+/*Ordenada*/
+void imprimir_versao_2(NoArv*raiz){
+    if(raiz){
+        imprimir_versao_2(raiz->esquerda);
+        printf("%d ",raiz->valor);
+        imprimir_versao_2(raiz->direita);
+    }
+}
+
 int main(void)
 {
     NoArv *raiz = NULL;
-
-    raiz = inserir_versao_1(raiz, 50);
+    int opcao,valor;
+    do {
+        printf("\nt0 - Sair\n\t1 - Inserir\n\t2 - Imprimir\n");
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 1:
+                printf("Digite um valor: "); // Corrigido o "print" (veja o próximo erro)
+                scanf("%d", &valor);     // Adicionado para ler o valor
+                raiz = inserir_versao_1(raiz, valor);
+                break;
+            case 2:
+                printf("Primeira impressão:\n");
+                imprimir_versao_1(raiz);
+                printf("\n");
+                printf("Segunda impressão:\n");
+                imprimir_versao_2(raiz);
+                printf("\n");
+                break;
+            default:
+                if (opcao != 0) {
+                    printf("Invalido\n"); // Adicionado "\n" para melhor formatação
+                }
+                break; // Boa prática: adicionar break no default
+        }
+    } while (opcao != 0);
 
     return 0;
 }
